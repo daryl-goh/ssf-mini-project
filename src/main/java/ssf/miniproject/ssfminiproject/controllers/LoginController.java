b0cecaa710f1258321292f55eb7b511b5826cbe8
daryl-goh <goh.daryl@gmail.com>
2022-10-07 20:27:16 +0800

    fixed Login/Ticker Tracker

--- src/main/java/ssf/miniproject/ssfminiproject/controllers/LoginController.java
+++ src/main/java/ssf/miniproject/ssfminiproject/controllers/LoginController.java
@@ -1,17 +1,22 @@
 package ssf.miniproject.ssfminiproject.controllers;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.view.RedirectView;
 import ssf.miniproject.ssfminiproject.repositories.RedisRepository;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping(path={"/"})
@@ -33,46 +38,51 @@ public class LoginController {
 
 	//Username Entered
     @PostMapping(path={"/login2"})
-    public String postLogin(@RequestBody MultiValueMap<String, String> form, Model model) {
-        
+    public String postLogin(@RequestBody MultiValueMap<String, String> form, Model model, RedirectAttributes redirectAttributes) {
+
         String username = form.getFirst("username");
+        redirectAttributes.addFlashAttribute("username", username);
+        return "redirect:/showData";
+    }
+
+    @GetMapping(value = "/showData")
+    public String showData(HttpServletRequest request, Model model) {
+        String username;
+        Map<String, ?> flashMap = RequestContextUtils.getInputFlashMap(request);
+        username =  (String) flashMap.get("username");
+
         List<String> values = repository.get(username);
 
-		
-        
+        String symbol = "No data available";
+        if (values != null && values.size() > 0) {
+            symbol = values.stream().collect(
+                    Collectors.joining(", "));
+        }
         model.addAttribute("username", username);
-        model.addAttribute("symbol", values);
+        model.addAttribute("symbol", symbol);
         return "login2";
     }
 
-    @PostMapping(path={"/login3"})
-    public String postLogin2(@RequestBody MultiValueMap<String, String> form, Model model) {
-        
+    @PostMapping(value="/login3", params = "symbol=add")
+    public RedirectView addTicker(@RequestBody MultiValueMap<String, String> form, RedirectAttributes redirectAttributes) {
+
         String username = form.getFirst("username");
         String value = form.getFirst("list");
-        
+
         repository.add(username, value);
-		
-        model.addAttribute("username", username);
-        model.addAttribute("symbol", value);
-        return "login2";
+        redirectAttributes.addFlashAttribute("username", username);
+        return new RedirectView("/showData", true);
     }
 
-    
-    // @PostMapping(path={"/login2"})
-    // public String postLogin3(@RequestBody MultiValueMap<String, String> form, Model model) {
-        
-    //     String username = form.getFirst("username");
-    //     String value = form.getFirst("list");
-        
-    //     repository.remove(username, value);
-		
-    //     model.addAttribute("username", username);
-    //     model.addAttribute("symbol", value);
-    //     return "login2";
-    // }
-    
+    @PostMapping(value="/login3", params = "symbol=remove")
+    public RedirectView removeTicker(@RequestBody MultiValueMap<String, String> form, RedirectAttributes redirectAttributes) {
 
-    
+        String username = form.getFirst("username");
+        String value = form.getFirst("list");
+
+        repository.remove(username, value);
+        redirectAttributes.addFlashAttribute("username", username);
+        return new RedirectView("/showData", true);
+    }
 
 }
