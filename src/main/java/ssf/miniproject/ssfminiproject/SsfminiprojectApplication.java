bc5d7e8b26049d3278778ea55949030f25d03510
daryl-goh <goh.daryl@gmail.com>
2022-12-20 22:15:32 +0800

    Added New Redis Database

--- src/main/java/ssf/miniproject/ssfminiproject/SsfminiprojectApplication.java
+++ src/main/java/ssf/miniproject/ssfminiproject/SsfminiprojectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
-@SpringBootApplication(exclude = RedisAutoConfiguration.class)
+@SpringBootApplication
 public class SsfminiprojectApplication {
 
 	public static void main(String[] args) {
